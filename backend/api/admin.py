96aa6acf4b6e5c1781454cb086974c63b1b25a73
chirayurana <chirayurana@yahoo.com>
2024-04-15 17:18:30 +0545

    configure database and models migration/register model to admin panel

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import CustomUser , Budget , Subscription , Income , Expense
 
-# Register your models here.
+admin.site.register(CustomUser)
+admin.site.register(Budget)
+admin.site.register(Subscription)
+admin.site.register(Income)
+admin.site.register(Expense)
