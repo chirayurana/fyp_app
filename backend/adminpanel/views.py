336e5fce916dded213b807b0af5d6138333bd896
chirayurana <chirayurana@yahoo.com>
2024-04-20 19:14:54 +0545

    Add login redirect

--- backend/adminpanel/views.py
+++ backend/adminpanel/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate , login , logout
+from django.contrib.auth.decorators import login_required
 
-
-
+@login_required(login_url="/login")
 def admin_panel_home_page_view(request):
   return render(request , "index.html")
 
