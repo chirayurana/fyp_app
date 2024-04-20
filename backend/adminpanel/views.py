88628711b37356ae4a4ff933499116c1ad65d7d1
chirayurana <chirayurana@yahoo.com>
2024-04-20 22:08:35 +0545

    Admin panel view update

--- backend/adminpanel/views.py
+++ backend/adminpanel/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
+from accounts.models import CustomUser
+from api.models import Income , Expense
+from random import randint
 
 @login_required(login_url="/login")
 def admin_panel_home_page_view(request):
-  return render(request , "index.html")
+  number_of_users = CustomUser.objects.all().count()
+  total_transactions = Income.objects.all().count() + Expense.objects.all().count()
+  total_feedback = 0
+  content = {
+    "total_users" : CustomUser.objects.all().count(),
+    "total_transactions" : Income.objects.all().count() + Expense.objects.all().count(),
+    "total_feedback" : total_feedback,
+    "server_load" : randint(0,15)
+  }
+  return render(request , "index.html" , content)
 
 
 def users_page_view(request):
